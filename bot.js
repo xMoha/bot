efa7fdd6126d093c71d0b2d50371387626ef900a
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-01 10:28:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,20 +48,6 @@ if  (msg.content === 'هلا') {
 
 
 
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`<@323885452207587329> حمار  ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
 
 
 
